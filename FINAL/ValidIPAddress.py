4ff5e05e0772b5305f21ad198a68ebc3e67ea88c
MomorioUHT <spirezaza.janthai000@gmail.com>
2024-10-26 16:53:05 +0700

    Fix IP

--- FINAL/ValidIPAddress.py
+++ FINAL/ValidIPAddress.py
@@ -3,6 +3,7 @@ IP = str(input()).split(".")
 def isValidIpAddress(context: list):
     if len(context) != 4 : return "No"
     for i in context:
+        if (not i.isdigit()): return "No"
         if len(i) == 0 or int(i) > 255 or int(i) < 0: return "No"
     return "Yes"
 
